7a0170cfb0085d67e730a71debd18e898998668b
ravraw <ravinrawat.wd@gmail.com>
2017-12-02 20:13:21 +0000

    3 js practice files

---
+++ chorus-for.js
@@ -0,0 +1,7 @@
+var chorus = "Let's dance";
+var i = 0;
+for(let i = 0;i<10;i++){
+console.log(chorus);
+
+}
+console.log('Till the Sun goes down');
---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chorus = "Let's dance";
+var i =0;
+while (i < 10 ){
+  if(i===5){
+    console.log('* change Key')
+  }
+console.log(chorus);
+ i++ ;
+}
+console.log("Until the sun comes up!")
